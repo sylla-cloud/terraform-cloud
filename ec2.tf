9d5911f0096b7a1aaf31fb980d3b1cbb50268125
sylla-cloud <aboulayesylla66@gmail.com>
2025-02-21 14:23:10 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "af-south-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0e6b72e746c973dd6"
+    instance_type = "t3.micro"
+    key_name = "devops-dirane"
+    tags = {
+        Name = "ec2-dirane"
+    }
+} 
