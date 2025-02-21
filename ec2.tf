2b9e3338fbef05f7c18892857aaf3ff4dd2a2937
sylla-cloud <aboulayesylla66@gmail.com>
2025-02-21 15:25:03 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,6 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-053a45fff0a704a47"
   instance_type = "t3.micro"
-  key_name      = "devops-dirane"
   tags = {
     Name = "ec2-dirane"
   }
