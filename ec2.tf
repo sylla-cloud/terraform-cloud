81d52f9d6cbc005911593f39109a470f2cf9cca4
sylla-cloud <aboulayesylla66@gmail.com>
2025-02-21 14:57:29 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-    region = "af-south-1"
+  region     = "us-east-1"
+ 
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-0e6b72e746c973dd6"
-    instance_type = "t3.micro"
-    key_name = "devops-dirane"
-    tags = {
-        Name = "ec2-dirane"
-    }
-} 
+  ami           = "ami-0e6b72e746c973dd6"
+  instance_type = "t3.micro"
+  key_name      = "devops-dirane"
+  tags = {
+    Name = "ec2-dirane"
+  }
+}
